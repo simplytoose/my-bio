e44b286ba3d015f273aa29db7f709a942b1fb1e7
Artur <housedarkas@gmail.com>
2026-07-06 09:55:16 +0300

    123

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   typescript: {
+    // Ігнорувати помилки типізації під час збірки
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Ігнорувати помилки лінтера під час збірки
+    ignoreDuringBuilds: true,
   },
   images: {
     unoptimized: true,
